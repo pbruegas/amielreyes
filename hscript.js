9978e81e8e373f14e77061e790784092540aa027
pbruegas <162727871+pbruegas@users.noreply.github.com>
2024-04-04 12:37:27 +0800

    Add files via upload

---
+++ hscript.js
@@ -0,0 +1,60 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+  
+    // Call the function when the page loads
+    window.addEventListener('load', hideSignUpButton);
+
+  // Function to check user signup status and hide the button if signed up
+  function hideSignUpButton() {
+    // Logic to check if user is signed up
+
+    // Get the Sign Up button element by its ID
+
+    // Check if the user is signed up and hide the button if true
+  }
+
+  // Call the function when the page loads
+  window.addEventListener('load', hideSignUpButton);
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
